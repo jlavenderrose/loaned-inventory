d5128b0364eb327ddfe78c097325b3a725d03cef
jmoore <jmoore@jrmoore-dev.(none)>
2013-06-11 20:33:45 -0700

    Adds default Rails 3.2.13 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoanedInventory::Application.config.secret_token = '6c9f23b967169b7e63eaa8c18e6248e9437734725242e3c58b55bde24fdb3f18270107024946be338961fb82c724051da98fc9198840466bb2fa023ef29099b2'
