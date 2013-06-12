3faad7cf10ce0f78cb0bf2d0edfac26c4b17f537
Julian Moore <jrmoore@wpi.edu>
2013-06-12 13:16:54 -0700

    An InventoryObject may only be Loaned to one Loanee at a time

--- app/models/inventory_loan.rb
+++ app/models/inventory_loan.rb
@@ -6,9 +6,19 @@ class InventoryLoan < ActiveRecord::Base
   
   validates_associated :inventory_object
   validates_associated :loanee
+  validate :only_current_loan
   #validates only current loan for each inventory_object
   
-  def search(query)
-	
+  #is this loan current?
+  def current?
+	returned_date.nil?
+  end
+  
+  private
+  def only_current_loan
+	#if the attached object has another other current loans give an error
+	if (inventory_object.inventory_loans.select {|loan| loan.current? && loan != self}).count then
+		errors.add(:inventory_object, "An Inventory Object may only be loaned to one Loanee at a time!")
+	end
   end
 end
