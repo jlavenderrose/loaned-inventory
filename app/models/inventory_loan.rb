429c0356e4b906bad2a491ef19898a9ce44e0b12
Julian Moore <jrmoore@wpi.edu>
2013-06-12 13:20:11 -0700

    Fixes logic in InventoryLoan::only_current_loan
    previously an existing loan would fail its own validation check even though
    it was obviously valid since it had been created successfully

--- app/models/inventory_loan.rb
+++ app/models/inventory_loan.rb
@@ -1,13 +1,12 @@
 class InventoryLoan < ActiveRecord::Base
-  attr_accessible :loaned_date, :returned_date, :loanee_id, :inventory_object_id
+  attr_accessible :loaned_date, :returned_date, :loanee_id, :inventory_object_id, :loanee_name, :inventory_object_name
   
   belongs_to :inventory_object
   belongs_to :loanee
   
   validates_associated :inventory_object
   validates_associated :loanee
-  validate :only_current_loan
-  #validates only current loan for each inventory_object
+  validate :only_current_loan, :on => :create
   
   #is this loan current?
   def current?
@@ -15,9 +14,10 @@ class InventoryLoan < ActiveRecord::Base
   end
   
   private
+  #validates that there is only one current loan for each inventory_object
   def only_current_loan
 	#if the attached object has another other current loans give an error
-	if (inventory_object.inventory_loans.select {|loan| loan.current? && loan != self}).count then
+	if (inventory_object.inventory_loans.select {|loan| loan.current? && loan != self}).count != 0 then
 		errors.add(:inventory_object, "An Inventory Object may only be loaned to one Loanee at a time!")
 	end
   end
