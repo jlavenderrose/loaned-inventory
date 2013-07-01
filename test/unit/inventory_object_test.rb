fccceffaf5c8478b9ea6ed796af464d09d0a7b35
Julian Moore <jreedmoore@gmail.com>
2013-06-30 17:48:46 -0700

    Adds passing unit tests for InventoryObjects

--- test/unit/inventory_object_test.rb
+++ test/unit/inventory_object_test.rb
@@ -13,21 +13,56 @@ class InventoryObjectTest < ActiveSupport::TestCase
   
   #TODO, fix these, object is not valid either
   test 'id1 unique' do
-    object = InventoryObject.create(id1: "DELLN02")
-    object2 = InventoryObject.create(id1: "DELLN02")
-    assert(!object2.valid?)
+    object = InventoryObject.create(id1: inventory_objects(:one).id1,
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id)
+    assert(!object.valid?, "object w/o unique id2 not valid")
+    object2 = InventoryObject.create(id1: inventory_objects(:one).id1+"D",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id)
+    assert(object2.valid?, "object2 w/ unique id1 not valid")
   end
   
   test 'id2 unique' do
-    object = InventoryObject.create(id2: "DELLN02")
-    object2 = InventoryObject.create(id2: "DELLN02")
-    assert(object.valid)
-    assert(!object2.valid?)
+	object = InventoryObject.create(id1: inventory_objects(:one).id1+"B",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id,
+									id2: inventory_objects(:one).id2)
+    assert(!object.valid?, "object w/o unique id2 not valid")
+    object2 = InventoryObject.create(id1: inventory_objects(:one).id1+"B",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id,
+									id2: inventory_objects(:one).id2+"B")
+    assert(object2.valid?, "object2 w/ unique id2 not valid")
   end
   
   test 'id3 unique' do
-    object = InventoryObject.create(id3: "DELLN02")
-    object2 = InventoryObject.create(id3: "DELLN02")
-    assert(!object2.valid?)
+    object = InventoryObject.create(id1: inventory_objects(:one).id1+"C",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id,
+									id3: inventory_objects(:one).id3)
+    assert(!object.valid?, "object w/o unique id3 not valid")
+    object2 = InventoryObject.create(id1: inventory_objects(:one).id1+"C",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id,
+									id3: inventory_objects(:one).id3+"C")
+    assert(object2.valid?, "object2 w/ unique id3 not valid")
+  end
+  
+  test 'inventory loans' do
+	@inventory_object = InventoryObject.find(inventory_objects(:one))
+	assert(@inventory_object.inventory_loans.count != 0, "inventory_object :one has non-zero inventory loans")
+	assert(@inventory_object.loanees.count != 0, "inventory_object :one has non-zero loanees")
+  end
+  
+  test 'report entries' do
+	@inventory_object = InventoryObject.find(inventory_objects(:one))
+	assert(@inventory_object.report_entries.count != 0, "inventory_object :one has non-zero report entries")
+	assert(@inventory_object.report_entries.open?, "inventory_object :one has open report_entries")
+  end
+  
+  test 'report entries open?' do
+	@inventory_object = InventoryObject.create(id1: inventory_objects(:one).id1+"F",
+									inventory_object_version_id: inventory_objects(:one).inventory_object_version.id)
+	assert(!@inventory_object.report_entries.open?, "new inventory_object has no open report_entries")
+  end
+  
+  test 'inventory object search' do
+	res = InventoryObject.search(inventory_objects(:one).id1)
+	assert(res.present?, "InventoryObject.search returns present result")
   end
 end
