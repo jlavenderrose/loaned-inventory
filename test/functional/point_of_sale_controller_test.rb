6566ee61ced60d6cec0d008c77e31ce36fe41952
Julian Moore <jreedmoore@gmail.com>
2013-07-10 21:48:53 -0700

    Adds failing tests for point of sale
    p_o_s/create is rendering as p_o_s/tag...

--- test/functional/point_of_sale_controller_test.rb
+++ test/functional/point_of_sale_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PointOfSaleControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	setup do
+		@inventory_object = inventory_objects(:one)
+		session[:user_token] = administrators(:one).remember_token
+	end
+
+	test "should get create" do
+		get :create
+		assert_response :success
+		assert_template 'point_of_sale/create'
+	end
+
+	test "should get tag" do
+		get :tag
+		assert_response :success		
+		assert_template 'point_of_sale/tag'
+	end
+
+	test "should get lookup" do
+		get :lookup
+		assert_response :success
+		assert_template 'point_of_sale/lookup'
+	end
 end
