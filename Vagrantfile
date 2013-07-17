9680027c4657ba02712f3769e29d0eaf68b03291
Julian Moore <jreedmoore@gmail.com>
2013-07-16 22:29:24 -0700

    Re-adds working LAMP
    This time configured with vagrant-librarian-puppet etc.

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,10 @@
 
 Vagrant::Config.run do |config|
   config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.host_name = "inv-test"
+  config.vm.forward_port 80, 8080
+  
 
   # This shell provisioner installs librarian-puppet and runs it to install
   # puppet modules. After that it just runs puppet
--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,2 +1,28 @@
 node default {
+  # Configure mysql
+  class { 'mysql::server':
+    config_hash => { 'root_password' => '8ZcJZFHsvo7fINZcAvi0' }
+  }
+  include mysql::php
+
+  # Configure apache
+  class { apache: 
+	mpm_module => 'prefork'
+  }
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port    => '80',
+    docroot => '/var/www/test',
+    require => File['/var/www/test'],
+  }
+
+  # Configure Docroot and index.html
+  file { ['/var/www', '/var/www/test']:
+    ensure => directory
+  }
+
+  file { '/var/www/test/index.php':
+    ensure  => file,
+    content => '<?php echo \'<p>Hello World</p>\'; ?> ',
+  }
 }
