90d9e1a9613579a082181e65120f487a286a0670
Julian Moore <jreedmoore@gmail.com>
2013-07-17 22:28:27 -0700

    Hacks passenger installed with rbenv into working

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -6,31 +6,50 @@ node /test/ {
 	class { 'mysql::server':
 		config_hash => { 'root_password' => '8ZcJZFHsvo7fINZcAvi0' }
 	}
-	
-	#ruby installation
-	$ruby_build = ['build-essential', 'bison', 'openssl', 'libreadline6', 'libreadline6-dev', 
-				   'curl', 'git-core', 'zlib1g', 'zlib1g-dev', 'libssl-dev', 'libyaml-dev', 
-				   'libsqlite3-dev', 'sqlite3', 'libxml2-dev', 'libxslt1-dev', 'autoconf', 
-				   'libc6-dev', 'libncurses5-dev', 'automake', 'libtool', 'libmysqlclient-dev']
 				   
-    #package{ $ruby_build: ensure => latest }
+    $bundle_dep = ['sqlite3', 'libmysqlclient-dev', 'libsqlite3-dev']
+    $passenger_dep = ['libcurl4-openssl-dev','apache2-threaded-dev', 'libapr1-dev', 'libaprutil1-dev']
+    package{ $bundle_dep: ensure => latest }
+    package{ $passenger_dep: ensure => latest }
     
     $ruby_version = "1.9.3-p327"
+    $ruby_major = "1.9.1"
+    $deploy_home = "/home/vagrant"
     
     rbenv::install { "vagrant":
-		group => "vagrant"
+		group => "vagrant",
+		home => $deploy_home
 	}
 	rbenv::compile { $ruby_version:
 		user => "vagrant"
 	}
-	rbenv::gem { "puppet":
+	$gems = ['puppet', 'passenger']
+	rbenv::gem { $gems:
 		user => "vagrant",
 		ruby => $ruby_version
-	}
-    	
-	# apache
+	}->
 	class {'apache': 
 		default_vhost => false
+	}->
+	exec {
+	  'install-passenger':
+		command => 'passenger-install-apache2-module --auto',
+		path => "${deploy_home}/.rbenv/bin:${deploy_home}/.rbenv/versions/${ruby_version}/bin:/bin:/usr/bin",
+		user => 'root',
+		require => Package[$passenger_dep]
+	}
+	
+	file { '/var/www/inventory/.ruby-version':
+		ensure => file,
+		owner => 'vagrant',
+		group => 'vagrant',
+		content => $ruby_version
+	}
+	exec { 'bundle install':
+		cwd => "/var/www/inventory",
+		path => "${deploy_home}/.rbenv/bin:${deploy_home}/.rbenv/versions/${ruby_version}/bin:/bin:/usr/bin",
+		user => "vagrant",
+		require => [File['/var/www/inventory/.ruby-version'], Package[$bundle_dep]]
 	}
 	
 	apache::vhost { 'default':
@@ -44,8 +63,49 @@ node /test/ {
 			}
 		]
 	}
-	include apache::mod::passenger
+	#class { 'apache::mod::passenger':
+	#	lib => '/home/vagrant/.rbenv/versions/1.9.3-p327/lib/ruby/gems/1.9.1/gems/passenger-4.0.10/buildout/apache2/mod_passenger.so',
+	#	passenger_root => '/home/vagrant/.rbenv/versions/1.9.3-p327/lib/ruby/gems/1.9.1/gems/passenger-4.0.10',
+	#	passenger_ruby => '/home/vagrant/.rbenv/versions/1.9.3-p327/bin/ruby'
+	#}
 
+	#passenger grossness
+	   file { '/etc/apache2/mods-available/passenger.load':
+        ensure  => present,
+        content => "LoadModule passenger_module /home/vagrant/.rbenv/versions/1.9.3-p327/lib/ruby/gems/1.9.1/gems/passenger-4.0.10/buildout/apache2/mod_passenger.so",
+        owner   => '0',
+        group   => '0',
+        mode    => '0644',
+      }
+
+      file { '/etc/apache2/mods-available/passenger.conf':
+        ensure  => present,
+        content => "PassengerRoot /home/vagrant/.rbenv/versions/1.9.3-p327/lib/ruby/gems/1.9.1/gems/passenger-4.0.10
+PassengerDefaultRuby /home/vagrant/.rbenv/versions/1.9.3-p327/bin/ruby",
+        owner   => '0',
+        group   => '0',
+        mode    => '0644',
+      }
+
+      file { '/etc/apache2/mods-enabled/passenger.load':
+        ensure  => 'link',
+        target  => '/etc/apache2/mods-available/passenger.load',
+        owner   => '0',
+        group   => '0',
+        mode    => '0777',
+        require => File['/etc/apache2/mods-available/passenger.load'],
+        notify  => Service['httpd'],
+      }
+
+      file { '/etc/apache2/mods-enabled/passenger.conf':
+        ensure  => 'link',
+        target  => '/etc/apache2/mods-available/passenger.conf',
+        owner   => '0',
+        group   => '0',
+        mode    => '0777',
+        require => File['/etc/apache2/mods-available/passenger.conf'],
+        notify  => Service['httpd'],
+      }
 
 	# Configure Docroot and index.html
 	file { '/var/www':
