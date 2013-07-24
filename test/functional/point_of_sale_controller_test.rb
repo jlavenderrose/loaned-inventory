1cef72b1fe2caa268515ea85e1093bfbc09f2b63
Julian Moore <jreedmoore@gmail.com>
2013-07-23 21:32:05 -0700

    Adds failing tests for #38

--- test/functional/point_of_sale_controller_test.rb
+++ test/functional/point_of_sale_controller_test.rb
@@ -11,6 +11,21 @@ class PointOfSaleControllerTest < ActionController::TestCase
 		assert_response :success
 		assert_template 'point_of_sale/create'
 	end
+	
+	test "should do create" do
+		assert_difference "InventoryObject.count" do
+		post :create, :inventory_object => { :inventory_object_version_id => @inventory_object.inventory_object_version.id,
+											 id1: "a",
+											 id2: "a",
+											 id3: "a" },
+					  :pos_session => {id2: "1", id3: "1"}	 
+		end
+										
+		assert_response :success
+		assert_select "#inventory_object_id1[value='a']", 0
+		assert_select "#inventory_object_id2[value='a']", 0
+		assert_select "#inventory_object_id3[value='a']", 0
+	end
 
 	test "should get tag" do
 		get :tag
